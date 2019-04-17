c841add58f92ea9dafea08a691d0a18330f89a76
Alejandro Hahn <halexhg@gmail.com>
2019-04-16 19:53:14 -0600

    Game Over Screen

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -9,16 +9,25 @@ public class LifeManager : MonoBehaviour
     public int startingLives;
     private int lifeCounter;
     private Text lifeText;
+    private GameObject player;
+    public GameObject gameOverScreen;
 
     void Start()
     {
         lifeText = GetComponent<Text>();
         lifeCounter = startingLives;
+        player = GameManager.instance.Player;
     }
 
     void Update()
     {
         lifeText.text = "x " + lifeCounter;
+
+        if(lifeCounter <= 0)
+        {
+            gameOverScreen.SetActive(true);
+            player.gameObject.SetActive(false);
+        }
     }
 
     public void GiveLife()
